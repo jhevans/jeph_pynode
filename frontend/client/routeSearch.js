f5bab99569ed865135bc2ebb38dd5d0e6176d686
John Evans <johnh.evans@tradermedia.co.uk>
2015-10-24 17:21:50 +0100

    get request now being fired off and response shown on page

---
+++ frontend/client/routeSearch.js
@@ -0,0 +1,35 @@
+
+Template.routeSearch.events({
+    "submit .routeSearchForm": function (event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+        console.log("submitting!")
+
+        // Get value from form element
+        var from = event.target.fromInput.value;
+        var to = event.target.toInput.value;
+
+        // Insert a task into the collection
+        Session.set('from', from);
+        Session.set('to', to);
+        // Clear form
+
+        Meteor.call("getRoute", [from, to], function(error, response){
+            Session.set('route', response);
+        });
+    }
+});
+
+
+Template.routeSearch.helpers({
+    from: function(){
+        return Session.get('from')
+    },
+    to: function(){
+        return Session.get('to')
+
+    },
+    route: function(){
+        return Session.get('route');
+    }
+})
