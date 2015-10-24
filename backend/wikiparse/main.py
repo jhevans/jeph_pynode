0c10af2b3c3fe40932fdaf2f7afb2700783c4ac3
Ed Kent <edward.kent@autotrader.co.uk>
2015-10-24 23:09:21 +0100

    Remove limit

--- backend/wikiparse/main.py
+++ backend/wikiparse/main.py
@@ -9,7 +9,7 @@ __author__ = 'Edward.Kent'
 def run():
     graph = WikiLinksGraph()
     parser = parse.Parser(graph)
-    parser.whole_file_parse(settings.WIKIPEDIA_XML_FILEPATH_TEST, limit=10000)
+    parser.whole_file_parse(settings.WIKIPEDIA_XML_FILEPATH_TEST)
 
 if __name__ == '__main__':
     run()
