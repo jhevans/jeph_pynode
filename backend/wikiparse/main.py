5ade9a0992dee488954bfea4f94375106cb9f14d
Ed Kent <edward.kent@autotrader.co.uk>
2015-10-24 23:11:09 +0100

    Use full snippet

--- backend/wikiparse/main.py
+++ backend/wikiparse/main.py
@@ -9,7 +9,7 @@ __author__ = 'Edward.Kent'
 def run():
     graph = WikiLinksGraph()
     parser = parse.Parser(graph)
-    parser.whole_file_parse(settings.WIKIPEDIA_XML_FILEPATH_TEST)
+    parser.whole_file_parse(settings.WIKIPEDIA_XML_FILEPATH_FULL)
 
 if __name__ == '__main__':
     run()
