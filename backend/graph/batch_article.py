144b88be9dfc0fc716d6b73e817af6f9d17dcfe2
Ed Kent <edward.kent@autotrader.co.uk>
2015-10-24 19:55:47 +0100

    fix: use file append

--- backend/graph/batch_article.py
+++ backend/graph/batch_article.py
@@ -41,7 +41,7 @@ class WikiArticleGraph(object):
         return result
 
     def do_batch_article_query(self, id, name):
-        with open(settings.BATCH_ARTICLE_FILE, 'w+') as batch_file:
+        with open(settings.BATCH_ARTICLE_FILE, 'a') as batch_file:
             batch_file.write("{},{}\n".format(id, name))
         self.query_count += 1
         if self.query_count >= self.batch_size:
