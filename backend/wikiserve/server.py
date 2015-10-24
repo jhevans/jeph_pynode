7775ee4a0b6bb24eaf92142078bd38c083fa63fc
Peter Ogden <peter.ogden@autotrader.co.uk>
2015-10-24 19:40:37 +0100

    Created server class with test and index methods

--- backend/wikiserve/server.py
+++ backend/wikiserve/server.py
@@ -1,2 +1,26 @@
 __author__ = 'Edward.Kent'
 
+import cherrypy
+
+test_string = 'Behold a string'
+
+class Server(object):
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def test_json(self):
+        return test_string
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def index(self, request=None): # Refactor to PUT
+        if request is not None:
+            reply = self._PUT_method(request)
+        return reply
+
+    def _PUT_method(self, request):
+        return test_string
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(Server())
