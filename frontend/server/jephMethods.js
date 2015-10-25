9e337a9713a09b0d422c9f0f3251e4b904799416
Hafeez.Mushtaq <hafeez.mushtaq@autotrader.co.uk>
2015-10-25 04:00:27 +0000

    add method to get WikiArticles By Title

--- frontend/server/jephMethods.js
+++ frontend/server/jephMethods.js
@@ -1,30 +1,7 @@
 Meteor.methods({
-    getLinkedArticles: function(pageid){
-        var pageid = "12";
-//        var response = HTTP.get("http://52.2.161.209:8080/?article_id=" + pageid);
-
-
-        //var expectedResponse = JSON.parse(Assets.getText('jsonContract/getLinkedArticlesResponse.json'));
-        var expectedResponse = {
-            "targetArticles": [
-                {
-                    "pageid": "43568",
-                    "name": "Tom_Hanks",
-                    "linkedArticles": [
-                        {
-                            "pageid": "41528",
-                            "name": "Forrest_Gump"
-                        },
-                        {
-                            "pageid": "946164",
-                            "name": "The_Green_Mile_(film)"
-                        }
-                    ]
-                }
-            ]
-        };
-
-        return expectedResponse.targetArticles[0].linkedArticles;
+    getLinkedArticles: function(from){
+        var response = HTTP.get("http://52.2.161.209:8080/related?title=" + from, {});
+        return response.data.linkedArticles;
     },
     getTargetArticle: function(){
         var pageid = "12";
